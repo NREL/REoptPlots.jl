ffe0fb5e6cbb4cf806b3c093b3d884c32e320af4
Byron Pullutasig <115118857+bpulluta@users.noreply.github.com>
2023-02-14 11:57:04 -0700

    updates

--- src/dispatch.jl
+++ src/dispatch.jl
@@ -58,7 +58,7 @@ function plot_electric_dispatch(d::Dict; title="Electric Systems Dispatch", save
     end
     
     key_list = ["PV","ElectricStorage","Generator","Wind","CHP","GHP"]
-    names_list = ["electric_to_load_series_kw", "storage_to_load_series_kw"]
+    #  = ["electric_to_load_series_kw", "storage_to_load_series_kw"]
     
     traces = GenericTrace[]
 
@@ -97,41 +97,42 @@ function plot_electric_dispatch(d::Dict; title="Electric Systems Dispatch", save
     for a_key in key_list
         if haskey(d, a_key)
             sub_dict = get(d, a_key, nothing)
-            for a_name in names_list
-                if haskey(sub_dict, a_name)
-                    data_array = get(sub_dict, a_name, nothing)
-                    if a_key == "ElectricStorage"
-                        data_array = get(sub_dict, names_list[2], nothing)
-                    end
-                    # Define an empty array to store the data arrays                    
-                    #invisible line for stacking
-                    push!(traces, scatter(
-                        name = "invisible",
-                        x = dr_v,
-                        y = total_array,
-                        mode="lines",
-                        fill = Nothing,
-                        line=attr(width=0),
-                        showlegend = false,
-                        hoverinfo = "skip"
-                    ))
+            data_array = []
+            empty(data_array)
+            
+            data_array = get(sub_dict, "electric_to_load_series_kw", nothing)
 
-                    add_array(data_array)
-                    total_array = create_total_array()
-
-                    #plot each technology
-                    push!(traces, scatter(
-                        name = a_key,
-                        x = dr_v,
-                        y = total_array,
-                        mode = "lines",
-                        fill = "tonexty",
-                        line=attr(width=0,color = color_list[current_color_index])
-                        ))        
-                    
-                        current_color_index = current_color_index + 1
-                end
+            if a_key == "ElectricStorage"
+                data_array = get(sub_dict, names_list[2], nothing)
             end
+
+            # Define an empty array to store the data arrays                    
+            #invisible line for stacking
+            push!(traces, scatter(
+                name = "invisible",
+                x = dr_v,
+                y = total_array,
+                mode="lines",
+                fill = Nothing,
+                line=attr(width=0),
+                showlegend = false,
+                hoverinfo = "skip"
+            ))
+
+            add_array(data_array)
+            total_array = create_total_array()
+
+            #plot each technology
+            push!(traces, scatter(
+                name = a_key,
+                x = dr_v,
+                y = total_array,
+                mode = "lines",
+                fill = "tonexty",
+                line=attr(width=0,color = color_list[current_color_index])
+                ))        
+            
+                current_color_index = current_color_index + 1
         end
     end
 
