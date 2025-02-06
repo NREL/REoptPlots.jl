a7cbe2e247a6f52d481b8c1d5f98f5350299f2d5
adfarth <adfarth@umich.edu>
2025-02-06 16:59:18 -0700

    fix colors

--- src/dispatch.jl
+++ src/dispatch.jl
@@ -59,7 +59,7 @@ function plot_electric_dispatch(d::Dict; title="Electric Systems Dispatch", save
         "electric_to_load_series_kw" => "RGBA(255, 89, 0, 1.0)", # Dark orange
         "electric_to_grid_series_kw" => "RGBA(255, 108, 82, 1.0)",
         "electric_to_storage_series_kw" => "RGBA(212, 23, 155, 1.0)",
-        "electric_curtailed_series_kw" => "RGBA(270, 108, 82, 1.0)"
+        "electric_curtailed_series_kw" => "RGBA(255, 210, 82, 1.0)"
     )
     colors["ElectricStorage"] = Dict(
         "storage_to_load_series_kw" => "#003A00",
